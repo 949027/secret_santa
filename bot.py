fa055c5901b347fee3d4f65e63000a65ff6ab298
949027 <82878668+949027@users.noreply.github.com>
2021-12-17 15:30:01 +0700

    Update

--- bot.py
+++ bot.py
@@ -7,14 +7,12 @@ from telegram.ext import CallbackContext
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
 from telegram import ReplyKeyboardMarkup
-from telegram import KeyboardButton
-import json
-import logging
-from datetime import datetime, timedelta
-#import telegramcalendar
+import phonenumbers
 import random
+from datetime import datetime
 
 states_database = {}
+
 
 def create_test_game(context):
     context.user_data['creator_telephone_number'] = '+79528949027'
@@ -26,9 +24,10 @@ def create_test_game(context):
     context.user_data['departure_date'] = '15.01.2022'
     context.user_data['game_id'] = 123456
 
+
 def chunks_generators(buttons, chunks_number):
     for button in range(0, len(buttons), chunks_number):
-        yield buttons[button : button + chunks_number]
+        yield buttons[button: button + chunks_number]
 
 
 def keyboard_maker(buttons, number):
@@ -67,8 +66,8 @@ def start(update, context):
 
     return 'SELECT_BRANCH'
 
+
 def select_branch(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
     if user_message == 'Создать игру':
@@ -82,7 +81,7 @@ def select_branch(update, context):
             text='Введи свой номер телефона или нажми кнопку',
             reply_markup=reply_markup,
         )
-        return 'GET_CONTACT'
+        return 'GET_CREATOR_CONTACT'
 
     if user_message == 'Вступить в игру':
         context.bot.send_message(
@@ -91,18 +90,18 @@ def select_branch(update, context):
         )
         return 'CHECK_GAME'
 
+
 def get_player_name(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
     context.user_data['player_name'] = user_message
 
-    buttons = ['Завершить']
+    buttons = ['Пропустить']
     markup = keyboard_maker(buttons, 1)
     context.bot.send_message(
         chat_id=chat_id,
         text=' Введи свой список желаний по одной вещи\n'
-             'Когда закончишь - нажми кнопку',
+             'или пропусти шаг',
         reply_markup=markup,
     )
     context.user_data['wish_list'] = []
@@ -110,22 +109,30 @@ def get_player_name(update, context):
 
 
 def get_wish_list(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
-    if user_message == 'Завершить':
+    if user_message == 'Завершить' or user_message == 'Пропустить':
+        buttons = ['Пропустить']
+        markup = keyboard_maker(buttons, 1)
         context.bot.send_message(
             chat_id=chat_id,
-            text='Теперь напиши письмо своему Санте',
+            text='Теперь напиши письмо своему Санте или пропусти шаг',
+            reply_markup=markup,
         )
         return 'GET_LETTER_FOR_SANTA'
     context.user_data['wish_list'].append(user_message)
     print(context.user_data['wish_list'])
+    buttons = ['Завершить']
+    markup = keyboard_maker(buttons, 1)
+    context.bot.send_message(
+        chat_id=chat_id,
+        text='Желание добавлено. Введи еще или нажми кнопку',
+        reply_markup=markup,
+    )
     return 'GET_WISH_LIST'
 
 
 def get_letter_for_santa(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
     context.user_data['letter'] = user_message
@@ -138,23 +145,40 @@ def get_letter_for_santa(update, context):
              'тайного друга. Ему и нужно будет подарить подарок!',
         reply_markup=markup,
     )
+    print(context.user_data)
     return 'SELECT_BRANCH'
 
 
+def check_telephone_number(number):
+    try:
+        parsed_number = phonenumbers.parse(number)
+        if phonenumbers.is_possible_number(parsed_number):
+            correct_number = f'+{parsed_number.country_code}' \
+                             f'{parsed_number.national_number}'
+            return correct_number
+        else:
+            return
+    except:
+        return
+
 
 def check_game(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
-    print(user_message, type(user_message))
-    print(context.user_data.get('game_id'), type(context.user_data.get('game_id')))
-    print(context.user_data)
-    if int(user_message) == context.user_data.get('game_id'):
+    print(int(user_message))
+    print(int(context.user_data.get('game_id')))
+    if int(user_message) == int(context.user_data.get('game_id')): #проверка есть ли игра с таким id
+        reply_markup = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton(
+            'Поделиться номером телефона',
+            request_contact=True,
+        )]])
+
         context.bot.send_message(
             chat_id=chat_id,
-            text=' Какое будет твоё имя в игре?',
+            text='Введи свой номер телефона или нажми кнопку',
+            reply_markup=reply_markup,
         )
-        return 'GET_PLAYER_NAME'
+        return 'GET_PLAYER_CONTACT'
     else:
         buttons = ['Создать игру', 'Вступить в игру']
         markup = keyboard_maker(buttons, 2)
@@ -167,9 +191,7 @@ def check_game(update, context):
 
 
 def show_game_info(update, context):
-    user = update.message.from_user
     chat_id = update.effective_message.chat_id
-    user_message = update.message.text
     context.bot.send_message(
         chat_id=chat_id,
         text=f'Название игры: {context.user_data["game_name"]}\n'
@@ -184,9 +206,8 @@ def show_game_info(update, context):
     return 'GET_PLAYER_NAME'
 
 
-def get_contact(update, context):
+def get_creator_contact(update, context):
     user = update.message.from_user
-    user_input = update.effective_message.text
     chat_id = update.effective_message.chat_id
     if update.message.contact is not None:
         user_phone_number = update.message.contact.phone_number
@@ -200,17 +221,54 @@ def get_contact(update, context):
     context.bot.send_message(
         chat_id=chat_id,
         text='Введите название игры',
-        reply_markup=telegram.ReplyKeyboardRemove()
+        reply_markup=telegram.ReplyKeyboardRemove(),
     )
 
     return 'GET_GAME_NAME'
 
 
-def create_game(update, context):
+def get_player_contact(update, context):
+    user = update.message.from_user
     chat_id = update.effective_message.chat_id
-    user_message = update.message.text
+    if update.message.contact is not None:
+        user_phone_number = update.message.contact.phone_number
+        context.user_data['player_telephone_number'] = user_phone_number
+    else:
+        user_phone_number = update.message.text
+        correct_phone_number = check_telephone_number(user_phone_number)
+        if not correct_phone_number:
+            reply_markup = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton(
+                'Поделиться номером телефона',
+                request_contact=True,
+            )]])
 
-    return 'GET_GAME_NAME'
+            context.bot.send_message(
+                chat_id=chat_id,
+                text='Неверно введен номер!\n'
+                     'Введи свой номер телефона в формате '
+                     '"+7 (123) 456-78-90" или нажми кнопку',
+                reply_markup=reply_markup,
+            )
+            return 'GET_PLAYER_CONTACT'
+        context.user_data['player_telephone_number'] = correct_phone_number
+        if correct_phone_number != user_phone_number:
+            context.bot.send_message(
+                chat_id=chat_id,
+                text=f'Ваш номер телефона: {correct_phone_number}',
+            )
+
+    context.user_data['player_first_name'] = user.first_name
+    context.user_data['player_username'] = user.username
+
+    buttons = [user.first_name]
+    markup = keyboard_maker(buttons, 1)
+    context.bot.send_message(
+        chat_id=chat_id,
+        text='Введите ваше имя в игре или нажмите кнопку',
+        reply_markup=markup,
+    )
+
+    return 'GET_PLAYER_NAME'
 
 
 def get_game_name(update, context):
@@ -221,7 +279,7 @@ def get_game_name(update, context):
     buttons = ['До 500 рублей',
                '500-1000 рублей',
                '1000-2000 рублей',
-    ]
+               ]
     markup = keyboard_maker(buttons, 3)
     context.bot.send_message(
         chat_id=chat_id,
@@ -231,17 +289,15 @@ def get_game_name(update, context):
     )
     return 'GET_COST_LIMIT'
 
+
 def get_cost_limit(update, context):
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
     context.user_data['cost_limit'] = user_message
-    buttons = ['25.12.2021', '31.12.2021']
-    markup = keyboard_maker(buttons, 2)
     context.bot.send_message(
         chat_id=chat_id,
         text='Введите дату окончания регистрации участников'
              ' (до 12.00 МСК) или нажмите одну из кнопок',
-        reply_markup=markup,
     )
     return 'GET_REGISTRATION_PERIOD'
 
@@ -249,35 +305,58 @@ def get_cost_limit(update, context):
 def get_registration_period(update, context):
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
-    context.user_data['registration_period'] = user_message
 
-    # здесь будет формирование календаря
-    context.bot.send_message(
-        chat_id=chat_id,
-        text='Дата отправки подарка?',
-        #reply_markup=calendar,
-    )
+    try:
+        registration_period = datetime.strptime(user_message, '%d-%m-%Y')
+        context.user_data['registration_period'] = registration_period
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Дата отправки подарка?',
+        )
+        return 'GET_DEPARTURE_DATE'
 
-    return 'GET_DEPARTURE_DATE'
+    except ValueError:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Неверная дата!\n'
+                 'Введи дату в формате ДД-ММ-ГГГГ, например - 31-12-2021',
+        )
+        return 'GET_REGISTRATION_PERIOD'
 
 
 def get_departure_date(update, context):
     chat_id = update.effective_message.chat_id
     user_message = update.message.text
-    context.user_data['departure_date'] = user_message
-    buttons = ['Получить ссылку для регистрации']
-    markup = keyboard_maker(buttons, 1)
-    context.bot.send_message(
-        chat_id=chat_id,
-        text='Отлично, Тайный Санта уже готовится к раздаче подарков!',
-        reply_markup=markup
-    )
-    return 'CREATE_REGISTRATION_LINK'
+
+    try:
+        departure_date = datetime.strptime(user_message, '%d-%m-%Y')
+        context.user_data['departure_date'] = departure_date
+        if departure_date <= context.user_data['registration_period']:
+            context.bot.send_message(
+                chat_id=chat_id,
+                text='Неверная дата!\n'
+                     f'Дата должна быть быть позже окончания регистрации',
+            )
+            return 'GET_DEPARTURE_DATE'
+        buttons = ['Получить ссылку для регистрации']
+        markup = keyboard_maker(buttons, 1)
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Отлично, Тайный Санта уже готовится к раздаче подарков!',
+            reply_markup=markup
+        )
+        return 'CREATE_REGISTRATION_LINK'
+    except ValueError:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Неверная дата!\n'
+                 'Введи дату в формате ДД-ММ-ГГГГ, например - 01-01-2022',
+        )
+        return 'GET_DEPARTURE_DATE'
 
 
 def create_registration_link(update, context):
     chat_id = update.effective_message.chat_id
-    user_message = update.message.text
     game_id = random.randint(111111, 999999)
     registration_link = f'https://t.me/secret_santa_devman_bot?start={game_id}'
 
@@ -299,7 +378,6 @@ def handle_user_reply(update: Update, context: CallbackContext):
     if update.message:
         user_reply = update.message.text
         chat_id = update.message.chat_id
-        user_id = update.message.from_user.id
 
     elif update.callback_query:
         user_reply = update.callback_query.data
@@ -313,9 +391,8 @@ def handle_user_reply(update: Update, context: CallbackContext):
         user_state = states_database.get(chat_id)
     states_functions = {
         'START': start,
-        'CREATE_GAME': create_game,
-        #'TELEPHONE_NUMBER_HANDLER': telephone_number_handler,
-        'GET_CONTACT': get_contact,
+        'GET_CREATOR_CONTACT': get_creator_contact,
+        'GET_PLAYER_CONTACT': get_player_contact,
         'GET_GAME_NAME': get_game_name,
         'GET_COST_LIMIT': get_cost_limit,
         'GET_REGISTRATION_PERIOD': get_registration_period,
@@ -336,14 +413,13 @@ def handle_user_reply(update: Update, context: CallbackContext):
 
 def main():
     load_dotenv()
-
     token = os.getenv("TELEGRAM_BOT_TOKEN")
+
     updater = Updater(token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', handle_user_reply))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_user_reply))
     dispatcher.add_handler(MessageHandler(Filters.contact, handle_user_reply))
-    #dispatcher.add_handler(MessageHandler(Filters.location, handle_user_reply))
     dispatcher.add_handler(CallbackQueryHandler(handle_user_reply))
     updater.start_polling()
 
